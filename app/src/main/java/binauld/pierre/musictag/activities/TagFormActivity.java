82d08d289bc58daf90d3fe4342cf30e665b14b6e
Fleck74 <fleck74@hotmail.fr>
2014-12-18 18:33:05 +0100

    Merge branch 'feat/tagModification' of C:\Users\Quentin\Android\music-tag with conflicts.

--- app/src/main/java/binauld/pierre/musictag/activities/TagFormActivity.java
+++ app/src/main/java/binauld/pierre/musictag/activities/TagFormActivity.java
@@ -2,36 +2,17 @@ package binauld.pierre.musictag.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.gson.Gson;
-
-import org.jaudiotagger.audio.AudioFile;
-import org.jaudiotagger.tag.FieldDataInvalidException;
-import org.jaudiotagger.tag.FieldKey;
-import org.jaudiotagger.tag.Tag;
 
 import binauld.pierre.musictag.R;
 
 public class TagFormActivity extends Activity {
-    private AudioFile audio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_tag_form);
-        String jsonAudioFile= "";
         setContentView(R.layout.activity_tag_form);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            jsonAudioFile = extras.getString("song");
-        }
-        audio = new Gson().fromJson(jsonAudioFile, AudioFile.class);
-
     }
 
 
@@ -55,25 +36,5 @@ public class TagFormActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendConfirmation(){
-        final EditText txt_title = (EditText) findViewById(R.id.txt_title);
-        String title = txt_title.getText().toString();
-        final EditText txt_artist = (EditText) findViewById(R.id.txt_artist);
-        String artist = txt_artist.getText().toString();
-        final EditText txt_album = (EditText) findViewById(R.id.txt_album);
-        String album = txt_album.getText().toString();
-
-        Tag tags = audio.getTag();
-        try {
-            tags.setField(FieldKey.TITLE,title);
-            tags.setField(FieldKey.ARTIST,artist);
-            tags.setField(FieldKey.ALBUM,album);
-        } catch (FieldDataInvalidException e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT);
-            Log.e(this.getClass().toString(), e.getMessage(), e);
-        }
-        audio.setTag(tags);
     }
 }
